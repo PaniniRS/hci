6f0d876d7698b8904fa02250238b77c4d93f9366
PaniniRS <bubemanchester@live.com>
2025-01-24 23:53:38 +0100

    added moments and redirects

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,81 @@
 const addAvatarCard = document.querySelector("#addCard");
 const settingsButton = document.querySelector(".settings");
 
+const CARDMAX = 4;
+
 // disable selection on document
 document.addEventListener("selectstart", (e) => e.preventDefault());
 
 //add card
 addAvatarCard.addEventListener("click", () => {
-  addAvatarCard.animate(
-    [
-      { transform: "scale(1)" },
-      { transform: "scale(0.9)" },
-      { transform: "scale(1)" },
-    ],
-    {
-      duration: 100,
+  if (!(document.querySelectorAll("#avatarCard").length >= CARDMAX)) {
+    addAvatarCard.animate(
+      [
+        { transform: "scale(1)" },
+        { transform: "scale(0.9)" },
+        { transform: "scale(1)" },
+      ],
+      {
+        duration: 100,
+        easing: "ease-in-out",
+        iterations: 1,
+      }
+    );
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.id = "avatarCard";
+    card.animate([{ transform: "scale(0)" }, { transform: "scale(1)" }], {
+      duration: 180,
       easing: "ease-in-out",
       iterations: 1,
-    }
-  );
+    });
 
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.animate([{ transform: "scale(0)" }, { transform: "scale(1)" }], {
-    duration: 180,
-    easing: "ease-in-out",
-    iterations: 1,
-  });
-
-  addAvatarCard.parentElement.appendChild(card);
+    addAvatarCard.parentElement.appendChild(card);
+  } else {
+    addAvatarCard.animate(
+      [
+        { transform: "translateX(0px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "translateX(-10px)" },
+        { transform: "translateX(10px)" },
+        { transform: "scale(1)" },
+        { transform: "scale(0.9)" },
+        { transform: "scale(1)" },
+      ],
+      {
+        duration: 100,
+        easing: "ease-in-out",
+        iterations: 1,
+      }
+    );
+  }
 });
 
 //settings
